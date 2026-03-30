a6dd544748738b7ce2d1fe4842bfe0caf0aa320a
ryanlorenz28 <ryanlorenz28@gmail.com>
2026-03-29 21:35:46 -0700

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,19 @@
+import logging
+import sys
+from colorama import Fore, Style
+
+class BotLogger:
+    def __init__(self, log_file="bot_log.txt", log_to_file=True):
+        self._logger = logging.getLogger("KalshiBot")
+        self._logger.setLevel(logging.DEBUG)
+        if not self._logger.handlers:
+            ch = logging.StreamHandler(sys.stdout)
+            ch.setFormatter(logging.Formatter("%(message)s"))
+            self._logger.addHandler(ch)
+            if log_to_file:
+                fh = logging.FileHandler(log_file, encoding="utf-8")
+                fh.setFormatter(logging.Formatter("%(asctime)s | %(message)s"))
+                self._logger.addHandler(fh)
+    def info(self, msg): self._logger.info(msg)
+    def warning(self, msg): self._logger.warning(Fore.YELLOW + f"WARNING: {msg}" + Style.RESET_ALL)
+    def error(self, msg): self._logger.error(Fore.RED + f"ERROR: {msg}" + Style.RESET_ALL)
